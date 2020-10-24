e7e3a5b08c77590fb211956e54723d31a228b577
Koh Meng Wang <mengwangk@gmail.com>
2020-10-24 16:00:02 +0800

    Initial commit

---
+++ hello_kedro.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+"""Contents of hello_kedro.py"""
+from kedro.io import DataCatalog, MemoryDataSet
+from kedro.pipeline import node, Pipeline
+from kedro.runner import SequentialRunner
+
+# Prepare a data catalog
+data_catalog = DataCatalog({"example_data": MemoryDataSet()})
+
+# Prepare first node
+def return_greeting():
+    return "Hello"
+
+
+return_greeting_node = node(return_greeting, inputs=None, outputs="my_salutation")
+
+# Prepare second node
+def join_statements(greeting):
+    return f"{greeting} Kedro!"
+
+
+join_statements_node = node(
+    join_statements, inputs="my_salutation", outputs="my_message"
+)
+
+# Assemble nodes into a pipeline
+pipeline = Pipeline([return_greeting_node, join_statements_node])
+
+# Create a runner to run the pipeline
+runner = SequentialRunner()
+
+# Run the pipeline
+print(runner.run(pipeline, data_catalog))
